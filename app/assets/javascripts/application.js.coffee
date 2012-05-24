0dfd8e188475fb9e2de3a560fe2d980450188623
Jack Flintermann <jflinter11@gmail.com>
2012-05-24 01:35:07 -0700

    landing page

---
+++ app/assets/javascripts/application.js.coffee
@@ -0,0 +1,11 @@
+//= require jquery
+//= require jquery_ujs
+//= require init
+//= require bootstrap-tab
+//= require bootstrap-tooltip
+//= require bootstrap-popover
+//= require bootstrap-alert
+//= require bootstrap-button
+//= require bootstrap-carousel
+//= require jquery.html5-placeholder-shim
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AnxietyCare::Application.config.secret_token = 'baf5c5924e3c8e8cc895a914af1446632a47e1e8541f0c0ca798bc79f7e6cdb714f1beb7053c607d724595bcffebc5030f28d1fd22fb546b2ae049d03569468b'
